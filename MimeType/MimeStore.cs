95a127782b9e23bd9617d9ddb568661679d8fb74
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-16 15:49:40 +0330

    Create MimeStore.cs

---
+++ MimeType/MimeStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MimeType
+{
+    internal class MimeStore : Dictionary<string, string>
+    {
+        public MimeStore() : base(StringComparer.OrdinalIgnoreCase)
+        {
+
+        }
+    }
+}
