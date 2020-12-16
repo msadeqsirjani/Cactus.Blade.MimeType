66688d7d8089db0be6125839c2c1fedfdb487d97
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-16 17:16:31 +0330

    Create MimeTest.cs

---
+++ MimeType.Test/MimeTest.cs
@@ -0,0 +1,40 @@
+﻿using Cactus.Blade.MimeType;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MimeType.Test
+{
+    [TestClass]
+    public class MimeTest
+    {
+        [TestMethod]
+        public void MimeTypeTest1()
+        {
+            Mime.AddMimeResources(typeof(Content.MimeType));
+
+            var name = Mime.Get("ami");
+
+            Assert.AreNotEqual(name, "application/vnd.amiga.ami");
+
+        }
+
+        [TestMethod]
+        public void MimeTypeTest2()
+        {
+            IMime mime = new Mime();
+
+            var mimeType = mime.Get("3dml");
+
+            Assert.AreNotEqual(mimeType, "text/vnd.in3d.3dml");
+        }
+
+        [TestMethod]
+        public void MimeTypeTest3()
+        {
+            IMime mime = new Mime();
+
+            var mimeType = mime["z3"];
+
+            Assert.AreNotEqual(mimeType, "text/vnd.in3d.3dml");
+        }
+    }
+}
