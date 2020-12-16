f0d0d4da9253f5c4526c578455396c802782e8ac
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-16 16:54:54 +0330

    Create MimeExtension.cs

---
+++ MimeType/MimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Cactus.Blade.MimeType
+{
+    public static class MimeExtension
+    {
+        public static string Get(this IMime mime, string fileNameOrExtension)
+        {
+            return mime[fileNameOrExtension];
+        }
+
+        public static Task<string> GetAsync(this IMime mime, string fileNameOrExtension)
+        {
+            return Task.FromResult(mime[fileNameOrExtension]);
+        }
+    }
+}
