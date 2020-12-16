1b1bf0205130379402059cc247f3130e2411d91d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-16 17:22:29 +0330

    Create Program.cs

---
+++ MimeType.Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Cactus.Blade.MimeType;
+using System;
+
+namespace MimeType.Sample
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            //Adding Resource file 
+            Mime.AddMimeResources(typeof(Content.MimeType));
+
+            //Using static method get mime Type From the Resources
+            var name = Mime.Get("ami");
+            Console.WriteLine("ami = " + name);
+
+            IMime mimeType = new Mime();
+            var mimeType1 = mimeType.Get("3dml");
+            Console.WriteLine("3dml = " + mimeType1);
+
+            var mimeType2 = mimeType["z3"];
+            Console.WriteLine("z3 = " + mimeType2);
+        }
+    }
+}
