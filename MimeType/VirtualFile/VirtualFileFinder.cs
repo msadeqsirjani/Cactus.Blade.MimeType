bc0c7fde46643433395aa696b1188b769ba60341
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-16 16:55:21 +0330

    Create VirtualFileFinder.cs

---
+++ MimeType/VirtualFile/VirtualFileFinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Cactus.Blade.MimeType.VirtualFile
+{
+    internal class VirtualFileFinder
+    {
+        public static VirtualFileInfo Find(Type resourceType, string filePath)
+        {
+            var virtualFileInfo = new VirtualFileInfo(resourceType.Assembly);
+            virtualFileInfo.Get(".json").Read(filePath, "json");
+            return virtualFileInfo;
+        }
+
+        public static async Task<VirtualFileInfo> FindAsync(Type resourceType, string filePath)
+        {
+            var virtualFileInfo = new VirtualFileInfo(resourceType.Assembly);
+            await (await virtualFileInfo.GetAsync(".json")).ReadAsync(filePath, "json");
+            return virtualFileInfo;
+        }
+    }
+}
